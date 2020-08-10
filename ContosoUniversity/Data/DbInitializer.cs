8a71d1b46168a2ca2ff0e9792c6024777d5d205c
Shota <gladius.vand@gmail.com>
2020-08-10 13:03:22 +0400

    Added DbInitializer that seeds the database
    Added DbInitializer file and its corresponding Program.cs code.
    Added nullable Grade property in Enrollment model.

---
+++ ContosoUniversity/Data/DbInitializer.cs
@@ -0,0 +1,84 @@
+﻿using ContosoUniversity.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoUniversity.Data
+{
+    public class DbInitializer
+    {
+        public static void Initialize(ContosoContext context)
+        {
+            context.Database.EnsureCreated();
+
+            if (context.Students.Any())
+            {
+                return;
+            }
+
+            Student[] students = new Student[]
+            {
+                new Student{ FirstName = "Valer", LastName = "Gelbakhiani", EnrollmentDate = DateTime.Parse("2003-05-28") },
+                new Student{ FirstName = "Gubaz", LastName = "Sanikidze", EnrollmentDate = DateTime.Parse("2003-07-03") },
+                new Student{ FirstName = "Giorgi", LastName = "Jorjadze", EnrollmentDate = DateTime.Parse("2004-05-28") },
+                new Student{ FirstName = "Rati", LastName = "Giorgadze", EnrollmentDate = DateTime.Parse("1994-05-24") },
+                new Student{ FirstName = "Avto", LastName = "Kamikadze", EnrollmentDate = DateTime.Parse("2004-05-28") },
+                new Student{ FirstName = "Davit", LastName = "Akhvlediani", EnrollmentDate = DateTime.Parse("2004-05-28") },
+                new Student{ FirstName = "Giorgi", LastName = "Akhvlediani", EnrollmentDate = DateTime.Parse("1994-05-03") },
+                new Student{ FirstName = "Davit", LastName = "Gogolidze", EnrollmentDate = DateTime.Parse("2001-02-07") },
+                new Student{ FirstName = "Giorgi", LastName = "Gogua", EnrollmentDate = DateTime.Parse("2004-02-07") },
+            };
+
+            foreach (Student s in students)
+            {
+                context.Add(s);
+            }
+
+            context.SaveChanges();
+
+            Course[] courses = new Course[]
+            {
+                new Course{ CourseID = 2325, Title = "Mathematics", Credits = 7 },
+                new Course{ CourseID = 2326, Title = "Physics", Credits = 6 },
+                new Course{ CourseID = 2327, Title = "Chemistry", Credits = 7 },
+                new Course{ CourseID = 2345, Title = "History", Credits = 5 }
+            };
+
+            foreach (Course c in courses)
+            {
+                context.Add(c);
+            }
+
+            context.SaveChanges();
+
+            Enrollment[] enrollments = new Enrollment[]
+            {
+                new Enrollment{ StudentID = 1, CourseID = 2325, Grade = Grade.A },
+                new Enrollment{ StudentID = 1, CourseID = 2345, Grade = Grade.B },
+                new Enrollment{ StudentID = 2, CourseID = 2325, Grade = Grade.F },
+                new Enrollment{ StudentID = 2, CourseID = 2345, Grade = Grade.A },
+                new Enrollment{ StudentID = 2, CourseID = 2326, Grade = Grade.B },
+                new Enrollment{ StudentID = 3, CourseID = 2325, Grade = Grade.A },
+                new Enrollment{ StudentID = 4, CourseID = 2345, Grade = Grade.D },
+                new Enrollment{ StudentID = 4, CourseID = 2325 },
+                new Enrollment{ StudentID = 5, CourseID = 2326 },
+                new Enrollment{ StudentID = 6, CourseID = 2325, Grade = Grade.E },
+                new Enrollment{ StudentID = 7, CourseID = 2325, Grade = Grade.A },
+                new Enrollment{ StudentID = 7, CourseID = 2325, Grade = Grade.A },
+                new Enrollment{ StudentID = 8, CourseID = 2326, Grade = Grade.D },
+                new Enrollment{ StudentID = 8, CourseID = 2325, Grade = Grade.F },
+                new Enrollment{ StudentID = 8, CourseID = 2345 },
+                new Enrollment{ StudentID = 9, CourseID = 2326, Grade = Grade.A },
+
+            };
+
+            foreach (Enrollment e in enrollments)
+            {
+                context.Add(e);
+            }
+
+            context.SaveChanges();
+        }
+    }
+}
--- ContosoUniversity/Models/Enrollment.cs
+++ ContosoUniversity/Models/Enrollment.cs
@@ -5,11 +5,17 @@ using System.Threading.Tasks;
 
 namespace ContosoUniversity.Models
 {
+    public enum Grade
+    {
+        A, B, C, D, E, F
+    }
+
     public class Enrollment
     {
         public int EnrollmentID { get; set; }
         public int StudentID { get; set; }
         public int CourseID { get; set; }
+        public Grade? Grade { get; set; }
 
         public Student Student { get; set; }
         public Course Course { get; set; }
