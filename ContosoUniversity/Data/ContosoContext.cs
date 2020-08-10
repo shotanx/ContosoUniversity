b24e0784b662bc8cfb7ac3a4bf6e272456c8efaa
Shota <gladius.vand@gmail.com>
2020-08-10 10:08:07 +0400

    Created Entity DbContext and DbSets.
    Created corresponding models. Added DbContext service to Startup.cs and ConnectionString(s) to appsettings.json

---
+++ ContosoUniversity/Data/ContosoContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ContosoUniversity.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContosoUniversity.Data
+{
+    public class ContosoContext : DbContext
+    {
+        public ContosoContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Course> Courses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Student>().ToTable("Student");
+            modelBuilder.Entity<Enrollment>().ToTable("Enrollment");
+            modelBuilder.Entity<Course>().ToTable("Course");
+        }
+    }
+}
---
+++ ContosoUniversity/Models/Enrollment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoUniversity.Models
+{
+    public class Enrollment
+    {
+        public int EnrollmentID { get; set; }
+        public int StudentID { get; set; }
+        public int CourseID { get; set; }
+
+        public Student Student { get; set; }
+        public Course Course { get; set; }
+    }
+}
