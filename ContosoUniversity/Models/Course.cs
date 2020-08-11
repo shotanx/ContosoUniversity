864611ff57aa9b492b441d763454fe6446103b0a
Shota <gladius.vand@gmail.com>
2020-08-11 10:54:15 +0400

    Changed StudentController default actions
    Incorporated security best practice for Edit action.
    Changed Views.
    Added attributes for Student and Course models.

--- ContosoUniversity/Models/Course.cs
+++ ContosoUniversity/Models/Course.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
@@ -10,7 +11,12 @@ namespace ContosoUniversity.Models
     {
         [DatabaseGenerated(DatabaseGeneratedOption.None)]
         public int CourseID { get; set; }
+
+        [StringLength(60, MinimumLength = 2)]
+        [Required]
         public string Title { get; set; }
+
+        [Range(4, 7)]
         public int Credits { get; set; }
 
         public ICollection<Enrollment> Enrollments { get; set; }
--- ContosoUniversity/Models/Student.cs
+++ ContosoUniversity/Models/Student.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,8 +10,20 @@ namespace ContosoUniversity.Models
     public class Student
     {
         public int ID { get; set; }
+
+        [StringLength(60, MinimumLength = 2)]
+        [Display(Name = "First Name")]
+        [Required]
         public string FirstName { get; set; }
+
+        [StringLength(60, MinimumLength = 2)]
+        [Display(Name = "Last Name")]
+        [Required]
         public string LastName { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:yyyy - MM - dd}", ApplyFormatInEditMode = false)]
+        [Display(Name = "Enrollment Date")]
+        [DataType(DataType.Date)]
         public DateTime EnrollmentDate { get; set; }
 
         public ICollection<Enrollment> Enrollments { get; set; }
